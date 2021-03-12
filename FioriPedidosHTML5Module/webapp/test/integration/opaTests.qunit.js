8292f7f8545e31b1277bfafd765df2a488d2d39c
marcelogreg <marcelo_gregorio@yahoo.com>
2021-03-12 13:53:43 +0000

    adicionado

---
+++ FioriPedidosHTML5Module/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,13 @@
+/* global QUnit */
+
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function() {
+	"use strict";
+
+	sap.ui.require([
+		"ns/FioriPedidosHTML5Module/test/integration/AllJourneys"
+	], function() {
+		QUnit.start();
+	});
+});
